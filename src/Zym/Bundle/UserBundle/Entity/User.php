41e2a9e670a65e4197f43379719c92d8db1c382d
Geoffrey Tran <geoffrey.tran@rapp.com>
2012-04-30 15:56:52 -0500

    Added disc map for user

--- src/Zym/Bundle/UserBundle/Entity/User.php
+++ src/Zym/Bundle/UserBundle/Entity/User.php
@@ -32,6 +32,7 @@ use Doctrine\ORM\Mapping as ORM;
  *
  * @ORM\InheritanceType("SINGLE_TABLE")
  * @ORM\DiscriminatorColumn(name="object_type", type="string")
+ * @ORM\DiscriminatorMap({"Zym\Bundle\UserBundle\Entity\User" = "Zym\Bundle\UserBundle\Entity\User"})
  *
  * @UniqueEntity(fields="username", message="This email is already used.")
  * @UniqueEntity(fields="email",    message="This email is already used.")
