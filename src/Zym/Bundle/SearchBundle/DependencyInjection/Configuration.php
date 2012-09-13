1b677b50735d3493230cd2cfe856cd65a9e34156
Geoffrey Tran <geoffrey.tran@rapp.com>
2012-09-13 17:39:43 -0500

    Fixing missing use

--- src/Zym/Bundle/SearchBundle/DependencyInjection/Configuration.php
+++ src/Zym/Bundle/SearchBundle/DependencyInjection/Configuration.php
@@ -2,6 +2,7 @@
 
 namespace Zym\Bundle\SearchBundle\DependencyInjection;
 
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
 
